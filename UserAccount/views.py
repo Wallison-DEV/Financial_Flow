aa5d2f682482d91ef0c867648d0f34d31180b8a1
Wallison <wallisonzwka@gmail.com>
2026-02-19 10:20:18 -0300

    add-user-permissions-system

--- UserAccount/views.py
+++ UserAccount/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import UserModel, ProfileModel
+from .serializers import UserSerializer, ProfileSerializer
+from .permissions import IsOwner
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated(), IsOwner]
+
+    def get_queryset(self):
+        return UserModel.objects.filter(id=self.request.user.id)
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner] 
+
+    def get_queryset(self):
+        return ProfileModel.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- manager/urls.py
+++ manager/urls.py
@@ -1,31 +1,16 @@
-"""
-URL configuration for manager project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import include, path
 from django.contrib import admin
 
 from Accounts import urls as accounts_urls
 from currencies import urls as currencies_urls
 from CreditCard import urls as creditcard_urls
+from UserAccount import urls as user_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path("auth/", include("rest_framework.urls")),
+    path("auth/", include("rest_framework.urls", namespace="rest_framework")), 
     path("acc/", include(accounts_urls)),
     path("currencies/", include(currencies_urls)),
     path("cards/", include(creditcard_urls)),
+    path("user/", include(user_urls))
 ]
